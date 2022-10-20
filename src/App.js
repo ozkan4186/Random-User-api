6c8650cc9f3793687b7b33d37e819b7456ee3b37
özkan demir <ozkanndemir2009@gmail.com>
2022-10-20 12:29:10 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import "./App.css";
+import { useState, useEffect } from "react";
+import Assets from "./Assets";
+
+function App() {
+  const [user, setUser] = useState("");
+
+  const getUser = async () => {
+    const url = "https://randomuser.me/api/ ";
+
+    try {
+      const { data } = await axios(url);
+      console.log(data.results[0]);
+      setUser(data.results[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <div className="App">
+      <Assets user={user} getUser={getUser} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Assets.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Mail from './assets/email.svg'
+import Phone from './assets/phone.svg'
+import Location from './assets/location.svg'
+
+const Assets = ({user,getUser}) => {
+    console.log(user)
+    const{name,picture,phone,location,email,dob,registered}=user
+    console.log(name)
+  return (
+    <div className='container' >
+        <div className="first">
+                <img src={picture?.large} alt="" />
+                   <h2> {name?.title} {name?.first} {name?.last} </h2>
+        </div>
+        <div className="first">
+            <img src={Mail} width="25px" alt="" />
+           <p> {email} </p>
+           
+        </div>
+        <div className="first">
+          <img src={Phone } width="25px"  alt="" />
+        <p> {phone} </p>
+
+
+        </div>
+           <div className="first">
+              <img src={Location} width="25px"  alt="" />
+                 <p> {location?.state} {location?.country}  </p>
+             </div>
+
+             <div className="footer">
+                <p>  Age:{dob?.age} </p>
+                <br />
+                <p>  Registered Date:{registered?.date} </p>
+             </div>
+
+        <button  className='button' >Random user</button>
+ 
+
+    </div>
+ 
+
+  )
+}
+
+export default Assets
